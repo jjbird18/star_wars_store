daee804b9bf80eb07d9091b70e0f1de87f385054
Jason J Stutz <jjbird18@yahoo.com>
2017-04-02 00:22:21 -0400

    S3 work

--- lib/carrierwave/storage/fog.rb
+++ lib/carrierwave/storage/fog.rb
@@ -4,7 +4,7 @@ CarrierWave.configure do |config|
     provider:              'AWS',                        # required
     aws_access_key_id:     'AKIAIOPESORHM4S4FNMA ',                        # required
     aws_secret_access_key: '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',                        # required
-    region:                'eu-west-1',                  # optional, defaults to 'us-east-1'
+    region:                'us-east-1',                  # optional, defaults to 'us-east-1'
     host:                  's3.example.com',             # optional, defaults to nil
     endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
