88139d7f9d4b0014352c2672102ffe75b21dfa04
Jason J Stutz <jjbird18@yahoo.com>
2017-04-02 23:28:05 -0400

    AWS

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,20 @@
+if Rails.env.test? or Rails.env.development?
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.root = "#{Rails.root}/tmp"
+    config.cache_dir = "#{Rails.root}/tmp/images"
+  end
+else
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+      :provider => 'AWS',
+      :aws_access_key_id => ENV['AKIAITYD62EEF6U7I6EQ'],
+      :aws_secret_access_key => ENV['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'],
+      :region => ENV['us-east-1']
+    }
+    config.storage = :fog
+    config.fog_directory = ENV['webd435bucket4741']
+    config.asset_host = "#{ENV['AWS_S3_ASSET_URL']}/#{ENV['webd435bucket4741']}"
+  end
+end
--- lib/carrierwave/storage/fog.rb
+++ lib/carrierwave/storage/fog.rb
@@ -5,8 +5,7 @@ CarrierWave.configure do |config|
     aws_access_key_id:     'AKIAIOPESORHM4S4FNMA ',                        # required
     aws_secret_access_key: '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',                        # required
     region:                'us-east-1',                  # optional, defaults to 'us-east-1'
-    host:                  's3.example.com',             # optional, defaults to nil
-    endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+  
   }
   config.fog_directory  = 'starwarsstorebucket'                          # required
   config.fog_public     = false                                        # optional, defaults to true
