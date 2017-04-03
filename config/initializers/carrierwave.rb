c9bd9102e2691afc0dea57ad3b8e9439a625b049
Jason J Stutz <jjbird18@yahoo.com>
2017-04-03 00:18:08 -0400

    AWS...again

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,12 +9,12 @@ else
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       :provider => 'AWS',
-      :aws_access_key_id => ENV[''],
-      :aws_secret_access_key => ENV[''],
+      :aws_access_key_id => ENV['AKIAITYD62EEF6U7I6EQ'],
+      :aws_secret_access_key => ENV['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'],
       :region => ENV['us-east-1']
     }
     config.storage = :fog
-    config.fog_directory = ENV['']
-    config.asset_host = "#{ENV['AWS_S3_ASSET_URL']}/#{ENV['']}"
+    config.fog_directory = ENV['webd435bucket4741']
+    config.asset_host = "#{ENV['AWS_S3_ASSET_URL']}/#{ENV['webd435bucket4741']}"
   end
 end
