cb2de9f5a66fef4fc621921e2c983e9a0ddc24ef
Jason J Stutz <jjbird18@yahoo.com>
2017-04-07 14:35:11 -0400

    Added admin priv.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170404232718) do
+ActiveRecord::Schema.define(version: 20170407182549) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20170404232718) do
     t.string   "country"
     t.string   "city"
     t.string   "state"
+    t.integer  "admin"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
